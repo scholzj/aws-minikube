b859262e23efca6549c920b7ee28f8591545bb2f
Jakub Scholz <www@scholzj.com>
2017-09-26 23:53:32 +0200

    Fix typo

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "minikube" {
   aws_region          = "${var.aws_region}"
   cluster_name        = "${var.cluster_name}"
   aws_instance_type   = "${var.aws_instance_type}"
-  ssh_public_key      = "${var.ssh_public_ley}"
+  ssh_public_key      = "${var.ssh_public_key}"
   aws_subnet_id       = "${var.aws_subnet_id}"
   hosted_zone         = "${var.hosted_zone}"
   hosted_zone_private = "${var.hosted_zone_private}"
